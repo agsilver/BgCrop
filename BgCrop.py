acbbcbcb972bcddfa3ae44fc8d0164b4b45d8748
Eyal <eyal.ver2@Gmail.com>
2020-05-15 18:32:50 +0300

    Version 1.0 is ready
    The first full version of the program.
    View Readme for more details.

--- BgCrop.py
+++ BgCrop.py
@@ -1,19 +1,79 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May  8 17:53:43 2020
-
-@author: user
-"""
-
 from PIL import Image, ImageChops
+import os
+import re
 
-im = Image.open("test2.jpg")
+def imgCrop(img):
+    w, h = img.size
+    limit = 230
+    ycrop = 0
+    #get y coordinate
+    for i in range(h-100):
+        tenpx = [img.getpixel((500, pixel)) for pixel in range(i,i+10)]
+        avgR = 0
+        avgG = 0
+        avgB = 0
+        for px in tenpx:
+            avgR += px[0]/10
+            avgG += px[1]/10
+            avgB += px[2]/10
+        # avgRGB = (avgR, avgG, avgB)
+        if (avgR < limit) and (avgG < limit) and (avgB < limit):
+            ycrop = i+50
+            break
 
-w = im.size[0] - 1
-h = im.size[1] - 1
-bg = Image.new(im.mode, im.size, im.getpixel((w,h)))
-diff = ImageChops.difference(bg, im)
-diff = ImageChops.add(diff, diff, 2.0, -100)
-bbox = diff.getbbox()
-im = im.crop(bbox)
-im.show()
+    # get x coordinate
+    for i in range(w-100):
+        tenpx = [img.getpixel((pixel, 500)) for pixel in range(i,i+10)]
+        avgR = 0
+        avgG = 0
+        avgB = 0
+        for px in tenpx:
+            avgR += px[0]/10
+            avgG += px[1]/10
+            avgB += px[2]/10
+        # avgRGB = (avgR, avgG, avgB)
+        if (avgR < limit) and (avgG < limit) and (avgB < limit):
+            xcrop = i+10
+            break
+
+    # get w coordinate
+    for i in range(w-100):
+        tenpx = [img.getpixel((w-1-pixel, 500)) for pixel in range(i,i+10)]
+        avgR = 0
+        avgG = 0
+        avgB = 0
+        for px in tenpx:
+            avgR += px[0]/10
+            avgG += px[1]/10
+            avgB += px[2]/10
+        # avgRGB = (avgR, avgG, avgB)
+        if (avgR < limit) and (avgG < limit) and (avgB < limit):
+            wcrop = i+10
+            break
+
+    # get h coordinate
+    for i in range(h-100):
+        tenpx = [img.getpixel((500, h-1-pixel)) for pixel in range(i,i+10)]
+        avgR = 0
+        avgG = 0
+        avgB = 0
+        for px in tenpx:
+            avgR += px[0]/10
+            avgG += px[1]/10
+            avgB += px[2]/10
+        # avgRGB = (avgR, avgG, avgB)
+        if (avgR < limit) and (avgG < limit) and (avgB < limit):
+            hcrop = i+10
+            break
+
+    crp = img.crop((0+xcrop,0+ycrop,w-wcrop,h-hcrop))
+    return crp
+
+path = os.path.abspath(os.path.dirname((__file__)))
+images = os.listdir(path+"\\Photos\\")
+currImg = ''
+for image in images:
+    photo = path + "\\Photos\\" + image
+    img = Image.open(photo)
+    crpImg = imgCrop(img)
+    crpImg.save(path + "\\Cropped\\" + re.sub(".jpg","",image) + "_crp.jpg")
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# BgCrop
-Automatic image background cropping tool
+## BgCrop
+Automatic image background cropping tool.
+NOTE: This tool was designed to remove white background only.
+
+This was developed for my father-in-law, who scans alot of old family photos, and was tired of having to crop
+each and every photo.
+
+# How to use:
+1. place all the photos you want to crop in the "BgCrop\Photos" folder. 
+2. double-click BgCrop.py
+3. Now all of your photos are cropped and placed in the "BgCrop\Cropped\" folder.
+
+# Dependencies:
+1. Pillow (PIL)
+2. RegEx (re)
